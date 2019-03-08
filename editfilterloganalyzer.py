a66de82ece83ee1f2fa85792e9fe7e547dc705f3
Galobtter <galobtter@gmail.com>
2019-03-08 21:39:17 +0530

    more options, work better

--- editfilterloganalyzer.py
+++ editfilterloganalyzer.py
@@ -7,13 +7,12 @@ query = p.data.api.ListGenerator("abuselog", aflprop = "details", aflfilter = ar
 query.set_maximum_items(args[2])
 query.set_query_increment(50)
 
-extra = r".{0,20}"
+extra = r".{,100}"
 pattern = regex.compile (r"(?V1)"+extra+r"(?:"+input("Regex: ")+r")"+extra, flags = regex.IGNORECASE)
 
 for hit in query:
 	details = hit["details"]
-	new_wikitext = details["new_wikitext"]
-	#added_lines = details.get("added_lines")
-	if new_wikitext:
-		match = pattern.search(new_wikitext)
-		if match: print(match.group())
+	var = details.get(args[3])
+	if var:
+		match = pattern.search(var)
+		if match: print(details["page_title"], match.group())
